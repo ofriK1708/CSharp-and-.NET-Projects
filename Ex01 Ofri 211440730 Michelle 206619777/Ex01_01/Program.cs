5a93c56810e6a457fd1ee0827fccba6f72dbe3d5
Ofrik17 <ofrikuperberg@gmail.com>
2024-12-07 17:48:38 +0200

    working bin to dec

--- Ex01 Ofri 211440730 Michelle 206619777/Ex01_01/Program.cs
+++ Ex01 Ofri 211440730 Michelle 206619777/Ex01_01/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ex01_01
 {
-    internal class Program
+    class Program
     {
+        public static void Main()
+        {
+            string binaryNumber1 = GetBinaryNumberFromUser(1);
+            string binaryNumber2 = GetBinaryNumberFromUser(2);
+            string binaryNumber3 = GetBinaryNumberFromUser(3);
+
+            int[] decimalNumbers = { convertBinryToDecimal(binaryNumber1), convertBinryToDecimal(binaryNumber2), 
+                convertBinryToDecimal(binaryNumber3) };
+
+            Array.Sort(decimalNumbers);
+            Console.WriteLine(@"The decimal numbers are: {0}, {1}, {2}", decimalNumbers[0], decimalNumbers[1], decimalNumbers[2]);
+        }
+
+        public static string GetBinaryNumberFromUser(int i_inputIndex)
+        {
+            Console.WriteLine("Please enter the #{0} binary number with 7 digits (and then press 'enter')", i_inputIndex);
+            string binaryNumber = Console.ReadLine();
+
+            while (!validateBinaryNumber(binaryNumber))
+            {
+                Console.WriteLine("the number you inserted isn't valid, please try again");
+                binaryNumber = Console.ReadLine();
+            }
+
+            return binaryNumber;
+        }
+
+        private static bool validateBinaryNumber(string i_binaryNumberToCheck)
+        {
+            bool validation = i_binaryNumberToCheck.Length == 7 && i_binaryNumberToCheck.All(c => c == '1' || c == '0');
+
+            return validation;
+        }
+
+        private static int convertBinryToDecimal(string i_binaryNumberToConvert)
+        {
+            int powerOf2 = 1;
+            int decimalNumber = 0;
+            for(int i = i_binaryNumberToConvert.Length - 1; i >= 0; i--) 
+            {
+                decimalNumber += int.Parse(i_binaryNumberToConvert[i].ToString()) * powerOf2;
+                powerOf2 *= 2;
+            }
+
+            return decimalNumber;
+        }
     }
 }
