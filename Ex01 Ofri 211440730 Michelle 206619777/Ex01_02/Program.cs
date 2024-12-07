27002082bcbf19096784dd577e10a62ab3714abf
Ofrik17 <ofrikuperberg@gmail.com>
2024-12-07 20:06:50 +0200

    Ex_02

---
+++ Ex01 Ofri 211440730 Michelle 206619777/Ex01_02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+namespace Ex01_02
+{
+    class Program
+    {
+        public static void Main()
+        {
+            printABCTree(100);
+        }
+
+        public static void printABCTree(int i_treeHeight = 7)
+        {
+            printABCTreeHelper(i_treeHeight, 1, 'A');
+        }
+
+        private static void printABCTreeHelper(int i_treeHeight,int i_lineNumber,char i_nextCharInLine)
+        {
+            if (i_lineNumber > i_treeHeight) 
+            {
+                return;
+            }
+
+            if(i_lineNumber >= i_treeHeight - 1)
+            {
+                int spacesInLine = i_treeHeight - 2;
+                StringBuilder lineToPrint = new StringBuilder();
+                lineToPrint.Append(i_lineNumber);
+                lineToPrint.Append(' ', spacesInLine);
+                lineToPrint.Append('|').Append(i_nextCharInLine).Append('|');
+                Console.WriteLine(lineToPrint);
+                printABCTreeHelper(i_treeHeight, i_lineNumber + 1, i_nextCharInLine);
+            }
+            
+            else
+            {
+                int charsInLine = (i_lineNumber * 2) - 1;
+                int spacesInLine = i_treeHeight - i_lineNumber;
+                StringBuilder lineToPrint = new StringBuilder();
+                lineToPrint.Append(i_lineNumber);
+                lineToPrint.Append(' ', spacesInLine);
+                for (int i =0; i <charsInLine; i++)
+                {
+                    if(i_nextCharInLine > 'Z')
+                    {
+                        i_nextCharInLine = 'A';
+                    }
+                    lineToPrint.Append(i_nextCharInLine);
+                    i_nextCharInLine++;
+                }
+
+                Console.WriteLine(lineToPrint);
+                printABCTreeHelper(i_treeHeight, i_lineNumber + 1, i_nextCharInLine++);
+            }
+        }
+    }
+}
