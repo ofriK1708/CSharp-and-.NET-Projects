1d17680f9f2fc9e53c996f6f7085f3a2652b26f2
Ofrik17 <ofrikuperberg@gmail.com>
2024-12-07 20:34:21 +0200

    Ex03

--- Ex01 Ofri 211440730 Michelle 206619777/Ex01_02/Program.cs
+++ Ex01 Ofri 211440730 Michelle 206619777/Ex01_02/Program.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 namespace Ex01_02
 {
-    class Program
+    public class Program
     {
         public static void Main()
         {
---
+++ Ex01 Ofri 211440730 Michelle 206619777/Ex01_03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex01_03
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int treeHeight = getAndValidateUserInput();
+            Ex01_02.Program.printABCTree(treeHeight);
+        }
+        private static int getAndValidateUserInput()
+        {
+            Console.WriteLine("Hello! please enter the tree height (the height should be between 2 and 15)");
+            string treeHeightFromUser = Console.ReadLine();
+            int treeHeight;
+            bool isValidNum = int.TryParse(treeHeightFromUser, out treeHeight);
+            while (!isValidNum || treeHeight > 15 || treeHeight < 2)
+            {
+                Console.WriteLine("Invalid height (the height should be between 2 and 15)");
+                treeHeightFromUser = Console.ReadLine();
+                isValidNum = int.TryParse(treeHeightFromUser, out treeHeight);
+            }
+
+            return treeHeight;
+        }
+    }
+}
