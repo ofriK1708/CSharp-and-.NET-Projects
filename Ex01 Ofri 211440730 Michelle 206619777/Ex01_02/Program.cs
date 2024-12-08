678097bbb23ee2cdc93bcb8946a9193a3146e929
michelle-skripnik <michelleskripnik@gmail.com>
2024-12-08 20:26:14 +0200

    ex04

--- Ex01 Ofri 211440730 Michelle 206619777/Ex01_02/Program.cs
+++ Ex01 Ofri 211440730 Michelle 206619777/Ex01_02/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 namespace Ex01_02
 {
--- Ex01 Ofri 211440730 Michelle 206619777/Ex01_05/Program.cs
+++ Ex01 Ofri 211440730 Michelle 206619777/Ex01_05/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 
 namespace Ex01_05
 {
