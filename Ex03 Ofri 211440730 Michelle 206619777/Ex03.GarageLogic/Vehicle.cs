19c410cb6fe9c1512f091a5ac168796a32e81b11
Ofri_k <deatlyligthen@gmail.com>
2025-01-06 01:04:32 +0200

    step1

--- Ex03 Ofri 211440730 Michelle 206619777/Ex03.GarageLogic/Vehicle.cs
+++ Ex03 Ofri 211440730 Michelle 206619777/Ex03.GarageLogic/Vehicle.cs
@@ -15,15 +15,33 @@ namespace ex03
         protected eVehicleType Type { set; get; }
 
         protected float m_EnergyPrecentage;
-        protected float MaxWheelAirPressure { set; get; }
+        protected float? MaxWheelAirPressure { set; get; } = null;
         protected int NumOfWheels { set; get; }
         protected Wheel[] m_Wheels;
         protected eEnergySourceType EnergySourceType { set; get; }
-        protected float EnergyMaxCapacity { set; get; }
+        protected float? EnergyMaxCapacity { set; get; } = null;
         protected float m_CurrentEnergyCapacity;
+        public float CurrentEnergyCapacity
+        {
+            get
+            {
+                return m_CurrentEnergyCapacity;
+            }
+            set
+            {
+                if(value < 0 || value > EnergyMaxCapacity.Value)
+                {
+                    throw new ValueOutOfRangeException(0, EnergyMaxCapacity.Value, "Current energy amount is out of the valid range");
+                }
+                else
+                {
+                    m_CurrentEnergyCapacity = value;
+                }
+            }
+        }
         protected eFuelType FuelType { set; get; }
 
-        public Vehicle(CostumerInfo i_CostumerAndVehcialInfo,string i_Model,string i_LicensePlate)
+        public Vehicle(CostumerInfo i_CostumerAndVehcialInfo, string i_Model, string i_LicensePlate)
         {
             m_CostumerInfo = i_CostumerAndVehcialInfo;
             Model = i_Model;
@@ -35,11 +53,11 @@ namespace ex03
             bool equals = false;
             Vehicle vehicleToCheck = obj as Vehicle;
 
-            if(vehicleToCheck != null) 
+            if (vehicleToCheck != null)
             {
                 equals = LicensePlate == vehicleToCheck.LicensePlate;
             }
-            
+
             return equals;
         }
         public override int GetHashCode()
@@ -50,10 +68,10 @@ namespace ex03
         {
             VehicleState = i_NewState;
         }
-        public void FillEnergyToFull() 
+        public void FillEnergyToFull()
         {
-            m_CurrentEnergyCapacity = EnergyMaxCapacity;
+            m_CurrentEnergyCapacity = EnergyMaxCapacity.Value;
         }
     }
-    
+
 }
--- Ex03 Ofri 211440730 Michelle 206619777/Ex03.GarageLogic/Wheel.cs
+++ Ex03 Ofri 211440730 Michelle 206619777/Ex03.GarageLogic/Wheel.cs
@@ -9,9 +9,35 @@ namespace ex03
     public struct Wheel
     {
         internal string ManufacturerName { get; set; }
-        internal float CurrentAirPressure { get; set; }
-        internal float  MaxAirPressure { get; set; }
+        internal float?  MaxAirPressure { get; set; }
+        private float m_CurrentAirPressure;
+        public float CurrentAirPressure
+        {
+            get
+            {
+                return m_CurrentAirPressure;
+            }
+            set
+            {
+                if (value < 0 || value > MaxAirPressure.Value)
+                {
+                    throw new ValueOutOfRangeException(0, MaxAirPressure.Value, "Current air pressure is out of the valid range");
+                }
+                else
+                {
+                    m_CurrentAirPressure = value;
+                }
+            }
+        }
+
+        public Wheel(string i_ManufacturerName,float i_MaxMaxAirPressure,float i_CurentAirPressure) 
+        {
+            ManufacturerName = i_ManufacturerName;
+            MaxAirPressure = i_MaxMaxAirPressure;
+            CurrentAirPressure = i_CurentAirPressure;
+
+        }
         
-        public fillAirToMax
+        
     }
 }
